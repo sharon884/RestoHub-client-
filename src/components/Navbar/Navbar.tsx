2f92227a879b566a4585570a7f44c3f9ae4967c6
sharon884 <sharonsharon9048461583@gmail.com>
2025-11-22 15:43:10 +0530

    feat(navbar): add navigation links for Home, Explore, and Add Restaurant pages
    Integrated React Router <Link> components into Navbar buttons
    
    Enabled navigation to /, /explore, and /add routes
    
    Improved overall UX by making top-level pages accessible from the header

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
 
 const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-zinc-100 shadow-md">
       <h1 className="text-xl font-bold">Resto Hub</h1>
       <div className="flex gap-4">
-        <Button>Home</Button>
-        <Button>Explore</Button>
-        <Button>Add Restaurant</Button>
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
+
+        <Link to="/explore">
+          <Button>Explore</Button>
+        </Link>
+        <Link to="/add">
+          <Button>Add Restaurant</Button>
+        </Link>
       </div>
     </nav>
   );
