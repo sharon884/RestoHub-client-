82975b45213d16aeb502072f589f5f22a63b6fa1
sharon884 <sharonsharon9048461583@gmail.com>
2025-11-22 15:56:24 +0530

    feat(routes): configure RestaurantDetail dynamic route and import page component

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import ExploreRestaurants from "../pages/ExploreRestaurants";
 import AddRestaurant from "../pages/AddRestaurantPage";
+import RestaurantDetail from '../pages/RestaurantDetail'; 
 
 const AppRoutes: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const AppRoutes: React.FC = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/explore" element={<ExploreRestaurants />} />
       <Route path="/add" element={<AddRestaurant />} />
+      <Route path="/restaurant/:id" element={<RestaurantDetail />} />
     </Routes>
   );
 };
